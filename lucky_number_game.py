dd997ca784deb2f82d205ccf2f7d5295c2573163
Ashmit Tiwari <ashmittiwwari@gmail.com>
2026-06-22 11:19:47 +0530

    Lucky_number_game

---
+++ lucky_number_game.py
@@ -0,0 +1,10 @@
+"""
+Problem Statement:
+
+Two players participate in the Lucky Number Challenge.
+Both players enter their names and the total number of rounds.
+If the total number of rounds is odd, Player 1 wins.
+If the total number of rounds is even, Player 2 wins.
+The program should display the winner and a congratulatory message.
+"""
+
