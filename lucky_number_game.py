7f25b5677c400c7fa1b8b628cf42b77ce3163596
Ashmit Tiwari <ashmittiwwari@gmail.com>
2026-06-22 11:20:39 +0530

    Lucky_number_game_v2

--- lucky_number_game.py
+++ lucky_number_game.py
@@ -8,3 +8,14 @@ If the total number of rounds is even, Player 2 wins.
 The program should display the winner and a congratulatory message.
 """
 
+print("-----THE------PICKUP-----GAME--------")
+player_1 = input("Enter the player name:")
+player_2 = input("Enter the player name: ")
+Total_Rounds = int(input("Enter the numbers of Rounds--"))
+print("--------PLAYER-1---START!!---First------- ")
+if Total_Rounds % 2 == 0:
+    print("player_2 Wins!!")
+    print("Congrats")
+else:
+    print("player_1 Wins!!")
+    print("Congrats")
